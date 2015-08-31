b23027c81f5fb1a25d3c0620702a7aaeb367c440
ViTr <ViTr@list.ru>
2015-08-31 21:48:28 +0400

    [*] Изучение JBDC

--- JavaMTIDynamic/src/ru/mti/edu/jdbc/Db.java
+++ JavaMTIDynamic/src/ru/mti/edu/jdbc/Db.java
@@ -6,8 +6,11 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
@@ -18,29 +21,54 @@ public class Db {
 
 	public static void main(String[] args) throws FileNotFoundException, SQLException {
 		try (Connection conn = getConnection()){
-			Statement stat = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-			stat.executeUpdate("delete from dept where deptno = 1");
+			DatabaseMetaData dbmd = conn.getMetaData();
+			
+			Statement stat = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+//			stat.executeUpdate("delete from dept where deptno = 1");
+			PreparedStatement prepStat = conn.prepareStatement("delete from dept where deptno = ?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+//			prepStat.setString(1, "1 or 1 = 1");
+			prepStat.setInt(1, 1);
+			// select * from dept where deptno = 1 or 1 = 1 - SQL Injection
+			prepStat.execute();
+//			System.out.println(prepStat.getUpdateCount());
 			stat.executeUpdate("insert into dept(deptno, dname, loc) values (1, 'IT Department', 'Moscow')");
 			ResultSet rs = stat.executeQuery("select * from dept");
+			ResultSetMetaData metaData = rs.getMetaData();
+			
 			rs.beforeFirst();
 			File file = new File("result.txt");
 			PrintWriter pw = new PrintWriter(file);
 			while(rs.next()){ 
-				String deptno = rs.getString("deptno");
+				int deptno = rs.getInt("deptno");
 				String dname = rs.getString("dname"),
 						loc = rs.getString(3); // rs.getString("loc")
-				pw.println(deptno + " | " + dname + " | " + loc);
+				if (deptno == 1){
+					rs.updateString("loc", "Saint-Peterburg");
+					rs.updateRow();
+				}
+				pw.println(deptno + " | " + dname + " | " + rs.getString(3));
 			}
-			pw.close();			
+			
+//			CallableStatement call = conn.prepareCall("{call raise_application_error(?, ?)}");
+//			call.setInt(1, -20002);
+//			call.setString(2, "Проверка вызова хранимой процедурыvgfgsf!");
+//			call.execute();
+			
+			pw.close();
+			stat.close();
+			prepStat.close();
 		}
 	}
 	
-	public static Connection getConnection(){
+	public static Connection getConnection() {
 		try {
 //			Class.forName("oracle.jdbc.driver.OracleDriver");
 			// �� �� ����� �������� �������� ����
 			OracleDriver driver = new OracleDriver();
 			DriverManager.registerDriver(driver);
+			
+//			DataSource dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/orcl");
+//			dataSource.getConnection();
 			
 			Properties prop = new Properties();
 			prop.load(new FileInputStream("src/ru/mti/edu/jdbc/dbinfo.properties"));
